9c71a2bac88706a7657c81381d6e49acce92a25a
wangcangeerr <wangcanger@outlook.com>
2024-07-30 10:34:35 +0800

    Initial commit

---
+++ src/main/java/com/wangcanger/blue_technique/BlueTechniqueClient.java
@@ -0,0 +1,10 @@
+package com.wangcanger.blue_technique;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlueTechniqueClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/wangcanger/blue_technique/BlueTechniqueDataGenerator.java
@@ -0,0 +1,11 @@
+package com.wangcanger.blue_technique;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BlueTechniqueDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
